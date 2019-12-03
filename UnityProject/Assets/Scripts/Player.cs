269a30dad5d053f7c6c520258799777c92a1594a
yi-rou <57440062+yi-rou@users.noreply.github.com>
2019-12-03 14:09:12 +0800

    已完成

---
+++ UnityProject/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEditor.UI;
+
+public class Player : MonoBehaviour
+{
+    [Header("血量")]
+    public float HP = 100;
+    [Header("攻擊力")]
+    public float attack ;
+    public Zombie zombie;
+    public AudioSource aud;
+    public AudioClip soundAttack;
+    private void Update()
+    {
+       Attack();
+        
+        if (Input.GetKeyDown(KeyCode.A) && zombie.HP <= 0)
+        {
+            print("殺死殭屍");
+        }
+       
+    }
+
+    private void Attack()
+    {
+        if (zombie.HP <= 0 || HP <= 0) return;
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            attack = Random.Range(10.9f, 25.6f);
+            zombie.HP = zombie.HP - attack;
+            print("殭屍受到傷害" + attack);
+            print("殭屍生下血量" + zombie.HP);
+            aud.PlayOneShot(soundAttack);
+        }
+
+    }
+
+}
---
+++ UnityProject/Assets/Scripts/Zombie.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    [Header("血量")]
+    public float HP = 100;
+    [Header("攻擊力")]
+    public float attack;
+    public Player player;
+    public AudioSource aud;
+    public AudioClip soundAttack;
+    private void Update()
+    {
+        Attack();
+        if (Input.GetKeyDown(KeyCode.B) && player.HP <= 0)
+        {
+            print("玩家死亡");
+        }
+    }
+
+    private void Attack()
+    {
+        if (player.HP <=0 || HP <= 0) return;
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            attack = Random.Range(9.9f,23.5f);
+            print("玩家受到傷害" + attack);
+            player.HP = player.HP - attack;
+            print("玩家剩下血量" + player.HP);
+            aud.PlayOneShot(soundAttack);
+        }
+    }
+
+}
